ec1fe2bde732274628687ed335b38e4db544cbc4
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-07-05 23:54:11 +0800

    Customized welcome message based on user info

--- prub-front/src/routes/+page.svelte
+++ prub-front/src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Hi Eklemis, Welcome to SvelteKit</h1>
+<script lang="ts">
+	import { page } from '$app/stores';
+	let session = $page.data.session;
+	let userName = session ? session.user?.name : 'There';
+</script>
+
+<h1>Hi {userName}, Welcome to SvelteKit</h1>
 <h2>Hope you will love the SvelteKit</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
